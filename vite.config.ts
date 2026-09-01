1404a29f1099cc1b96aa8d282db4a9b07a6ab0f2
devilbd <vladymir.parchev@gmail.com>
2026-09-01 10:42:10 +0300

    ....

--- vite.config.ts
+++ vite.config.ts
@@ -162,6 +162,7 @@ export default defineConfig(({ mode }) => {
         },
         build: {
             chunkSizeWarningLimit: 800,
+            modulePreload: false,
             rollupOptions: {
                 output: {
                     manualChunks(id: string) {
